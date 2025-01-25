c86316f3c70516931edd2ed9e3e07b12d0945914
William Li <52115161+Trollermaner@users.noreply.github.com>
2025-01-25 14:04:11 -0500

    redux + routes

---
+++ frontend/src/app/pages/NotFound.tsx
@@ -0,0 +1,20 @@
+// filepath: /Users/williamli/Desktop/Personal/Coding Projects/mchacks/frontend/src/app/pages/NotFound.tsx
+import React from 'react';
+import { Container, Typography, Box } from '@mui/material';
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Box textAlign="center" mt={10}>
+        <Typography variant="h1" component="h1" gutterBottom>
+          404 - Page Not Found
+        </Typography>
+        <Typography variant="h6" component="p">
+          Sorry, the page you are looking for does not exist.
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default NotFound;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
